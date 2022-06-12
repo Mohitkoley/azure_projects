9d84bb1de82583dd6b1e96b3748bc96abe4347a6
Mohit <mohitkoley5@gmail.com>
2022-06-12 14:39:52 +0530

    First commit

---
+++ az204-auth/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "438d3a94-405f-4dc3-9735-3fecaba6bad4";
+        private const string _tenantId = "7e9aacc0-700a-48a7-a5e7-2aeee880a8fd";
+
+        public static async Task Main(string[] args)
+        {
+            
+            var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+            string[] scopes = { "user.read" };
+            
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
---
+++ rediscache/Rediscache/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace Rediscache
+{
+    class Program
+    {
+        // connection string to your Redis Cache
+        static string connectionString = "5QI5AsWPCkdFMKamgtREjxegaTgDpVBp7AzCaOh6Pd4=";
+
+        static async Task Main(string[] args)
+        {
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+                {
+                    IDatabase db = cache.GetDatabase();
+
+                    // Snippet below executes a PING to test the server connection
+                    var result = await db.ExecuteAsync("ping");
+                    Console.WriteLine($"PING = {result.Type} : {result}");
+
+                    // Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+                    bool setValue = await db.StringSetAsync("test:key", "100");
+                    Console.WriteLine($"SET: {setValue}");
+
+                    // StringGetAsync takes the key to retrieve and return the value
+                    string getValue = await db.StringGetAsync("test:key");
+                    Console.WriteLine($"GET: {getValue}");
+
+                }
+        }
+    }
+}
